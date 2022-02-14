4bcf80cc908acb8a688a2fb5c48846212602145d
KatsiarynaMaslak <99599510+KatsiarynaMaslak@users.noreply.github.com>
2022-02-14 14:19:48 +0300

    Add files via upload

---
+++ meeting-room-2.0-android-main/app/src/main/java/com/meetingroom/android/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.meetingroom.android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meetingroom.android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ meeting-room-2.0-android-main/app/src/main/java/com/meetingroom/android/ui/SampleActivity.kt
@@ -0,0 +1,12 @@
+package com.meetingroom.android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meetingroom.android.R
+
+class SampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
